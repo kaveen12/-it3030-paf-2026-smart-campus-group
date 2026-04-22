25ae58d907be978735e077f8f2a58d76e10a82dc
Kaveen12 <kaveenchanaka12345@gmail.com>
2026-04-22 19:43:08 +0530

    User role back

---
+++ StatMind-backend/src/main/java/com/statmind/paf/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.statmind.paf.repository;
+
+import com.statmind.paf.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
+}
