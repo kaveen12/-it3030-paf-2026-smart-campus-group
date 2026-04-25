36b5915f73b0bbadc2e02185cea24ac24b9d9a36
Rathnayaka R. S. T. H <rathnayakatharukihimaya@gmail.com>
2026-04-26 00:13:51 +0530

    add nav bars
    add user and admin nav bars

--- StatMind frontend/src/App.jsx
+++ StatMind frontend/src/App.jsx
@@ -1,5 +1,6 @@
-// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AdminLayout } from "./components/AdminLayout";
+import { UserLayout } from "./components/UserLayout";
 import { TicketDashboard } from "./pages/TicketDashboard";
 import { CreateTicket } from "./pages/CreateTicket";
 import { TicketDetail } from "./pages/TicketDetail";
@@ -8,10 +9,62 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/tickets" element={<TicketDashboard />} />
-        <Route path="/tickets/create" element={<CreateTicket />} />
-        <Route path="/tickets/:ticketId" element={<TicketDetail />} />
-        <Route path="/" element={<TicketDashboard />} />
+        {/* USER ROUTES */}
+        <Route
+          path="/user/tickets"
+          element={
+            <UserLayout>
+              <CreateTicket />
+            </UserLayout>
+          }
+        />
+
+        <Route
+          path="/user/tickets/create"
+          element={
+            <UserLayout>
+              <CreateTicket />
+            </UserLayout>
+          }
+        />
+
+        <Route
+          path="/user/tickets/:ticketId"
+          element={
+            <UserLayout>
+              <TicketDetail />
+            </UserLayout>
+          }
+        />
+
+        {/* ADMIN ROUTES */}
+        <Route
+          path="/admin/tickets"
+          element={
+            <AdminLayout>
+              <TicketDashboard />
+            </AdminLayout>
+          }
+        />
+
+        <Route
+          path="/admin/tickets/:ticketId"
+          element={
+            <AdminLayout>
+              <TicketDetail />
+            </AdminLayout>
+          }
+        />
+
+        {/* DEFAULT */}
+        <Route
+          path="/"
+          element={
+            <UserLayout>
+              <CreateTicket />
+            </UserLayout>
+          }
+        />
       </Routes>
     </Router>
   );
