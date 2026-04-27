c25a75d8563ac447d35ef8b562f53ceb65f47d65
shehara2002 <sheharasavindi02@gmail.com>
2026-04-27 20:19:20 +0530

    update user nav

--- StatMind frontend/src/components/UserNavbar.jsx
+++ StatMind frontend/src/components/UserNavbar.jsx
@@ -36,7 +36,7 @@ const navItems = [
     ),
   },
   {
-    to: "/create-booking",
+    to: "/",
     label: "Bookings",
     icon: (
       <svg className="w-4 h-4" viewBox="0 0 24 24" fill="none">
