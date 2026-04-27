465a4a58c05b586f097570de4f89c0c8babc7914
Rathnayaka R. S. T. H <rathnayakatharukihimaya@gmail.com>
2026-04-27 18:38:24 +0530

    update ResourceRepository

--- StatMind-backend/src/main/java/com/statmind/paf/repository/ResourceRepository.java
+++ StatMind-backend/src/main/java/com/statmind/paf/repository/ResourceRepository.java
@@ -1,9 +1,19 @@
+// ============================================
+// REPOSITORY : ResourceRepository.java
+// package com.statmind.paf.repository
+// ============================================
 package com.statmind.paf.repository;
 
 import com.statmind.paf.model.Resource;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.Optional;
+import java.util.List;
+
 public interface ResourceRepository extends MongoRepository<Resource, String> {
+
+    List<Resource> findByResourceCodeStartingWithIgnoreCase(String prefix);
+    List<Resource> findByType(String type);
+
+    Optional<Resource> findByResourceCode(String resourceCode);
 }
